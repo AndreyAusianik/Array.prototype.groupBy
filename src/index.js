55d5d338b68c6dba1cc0eadf236b355a227133fd
Andrei Ausianik <andrei.ausianik@iii.com>
2018-01-20 13:56:08 +0300

    groupBy task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function groupBy(array) {
-    // your solution here
+module.exports = function groupBy(mapper = item => item) {
+    let answer = {};
+    this.forEach(element => {
+        let key = mapper(element);
+        let arr;
+        if(answer[key]) {
+            arr = answer[key]
+        } else {
+           answer[key] = arr = [];
+        }
+        arr.push(element);
+    });
+    return answer;
 }
